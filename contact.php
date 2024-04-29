197a95395db06c55d84579609f161823bcc01ca0
LouaySassi <sassi.louaycheker@etudiant-fst.utm.tn>
2024-04-29 22:07:08 +0100

    contact backend fixed

--- contact.php
+++ contact.php
@@ -1,47 +1,31 @@
 <?php
+$servername = "localhost";
+$username = "root"; // Replace with your database username
+$password = ""; // Replace with your database password
+$dbname = "web_db";
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-   
-    $name = strip_tags(trim($_POST["name"]));
-    $name = str_replace(array("\r","\n"),array(" "," "),$name);
-    $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
-    $message = trim($_POST["message"]);
 
-    // Check that data was sent to the mailer.
-    if ( empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        http_response_code(400);
-        echo "Oops! There was a problem with your submission. Please complete the form and try again.";
-        exit;
-    }
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-    // Set the recipient email address.
-    $recipient = "hachichamohamed10@gmail.com";
 
-    // Set the email subject.
-    $subject = "New contact from $name";
-
-    // Build the email content.
-    $email_content = "Name: $name\n";
-    $email_content .= "Email: $email\n\n";
-    $email_content .= "Message:\n$message\n";
-
-    // Build the email headers.
-    $email_headers = "From: $name <$email>";
-
-    // Send the email.
-    if (mail($recipient, $subject, $email_content, $email_headers)) {
-        // Set a 200 (okay) response code.
-        http_response_code(200);
-        echo "Thank You! Your message has been sent.";
-    } else {
-        // Set a 500 (internal server error) response code.
-        http_response_code(500);
-        echo "Oops! Something went wrong and we couldn't send your message.";
-    }
-
-} else {
-    // Not a POST request, set a 403 (forbidden) response code.
-    http_response_code(403);
-    echo "There was a problem with your submission, please try again.";
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
+
+
+$stmt = $conn->prepare("INSERT INTO contacts (name, email, message) VALUES (?, ?, ?)");
+$stmt->bind_param("sss", $name, $email, $message);
+
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+$stmt->execute();
+
+echo "Message sent successfully";
+header("Location: home.html");
+
+$stmt->close();
+$conn->close();
 ?>
